bb77b9179798a33ca3b169c3008dd76cb53a882d
Ali Hassan <93866104+rkhassan420@users.noreply.github.com>
2024-08-18 22:32:05 +0500

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,31 @@
+ 
+
+
+
+// profile password show, hide
+       
+    function show_pass() {
+        var x = document.getElementById('code');
+       
+
+        if (x.type === "password") {
+            x.type = "text";
+            
+        } else {
+            x.type = "password";
+              
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
